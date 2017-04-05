c635360d77f44e7cf644dc4adec0c300e5225156
S Stewart <xshotD@users.noreply.github.com>
2017-04-05 16:47:55 -0500

    Create web server (does nothing atm)

---
+++ www/server.js
@@ -0,0 +1,7 @@
+
+// Core web server (express for app, serve-static for serving files)
+const e = require('express');
+const s = require('serve-static');
+
+// TODO: Develop server (should be easy)
+// TODO: Show bot stats (may be a bit hard depending on if I can carry library stats to another file.)
