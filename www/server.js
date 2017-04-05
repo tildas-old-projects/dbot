4184a8ac6cb9b2af59acce865becf886b7ba738a
S Stewart <hi@youneeda.pw>
2017-04-05 17:04:53 -0500

    ugh

--- www/server.js
+++ www/server.js
@@ -1,7 +1,7 @@
 
 // Core web server (express for app, serve-static for serving files)
 const e = require('express');
-const s = require('serve-static');
+const s = require('serve');
 
 // TODO: Develop server (should be easy)
 // TODO: Show bot stats (may be a bit hard depending on if I can carry library stats to another file.)
