710a21e5737a05b212e2c722059a0d76318d911e
S Stewart <hi@youneeda.pw>
2017-04-12 17:30:21 -0500

    [core] Switch to StandardJS and add test

--- bot/core.js
+++ bot/core.js
@@ -1,23 +1,19 @@
-const e = require('eris');
+const e = require('eris')
 
-const fs = require('fs');
+const fs = require('fs')
 
 // Please edit config.json with your token.
-var conf = JSON.parse(fs.readFileSync('../config.json', 'utf8'));
+var conf = JSON.parse(fs.readFileSync('../config.json', 'utf8'))
 
 // You may change this desc, the owner, and prefix.
 const bot = new e.CommandClient(conf.token, {}, {
-    description: "A test bot made with Eris",
-    owner: "lold#4960",
-    prefix: "&&"
-});
+  description: 'A test bot made with Eris',
+  owner: 'lold#4960',
+  prefix: '&&'
+})
 
-bot.on("ready", () => {
-    console.log("dbot - discord responded with ready");
-    console.log("dbot stats:");
-    console.log("servers: ", bot.client.guilds);
-    console.log("total users: ", bot.client.users);
-});
+bot.on('ready', () => {
+  console.log('dbot - discord responded with ready')
+})
 
-
-bot.connect();
+bot.connect()
--- www/server.js
+++ www/server.js
@@ -1,7 +1,12 @@
 
-// Core web server (express for app, serve-static for serving files)
-const e = require('express');
-const s = require('serve');
-
+// Core web server (express for app, serve-static for serving files, and path for dir joining)
+const e = require('express')
+const s = require('serve')
+const p = require('path')
 // TODO: Develop server (should be easy)
 // TODO: Show bot stats (may be a bit hard depending on if I can carry library stats to another file.)
+
+const a = e()
+
+a.use(s(p.join(__dirname, 'views')))
+a.listen(3000)
