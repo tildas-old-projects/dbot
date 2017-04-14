30c361b112575afe60af10614eefee340036cd01
S Stewart <hi@youneeda.pw>
2017-04-14 14:57:44 -0500

    [page] stuff

--- bot/core.js
+++ bot/core.js
@@ -7,18 +7,18 @@ var conf = JSON.parse(fs.readFileSync('config.json', 'utf8'))
 
 // You may change this desc, the owner, and prefix.
 const bot = new e.CommandClient(conf.token, {}, {
-    description: 'A test bot made with Eris',
-    owner: 'lold#4960',
-    prefix: '&'
+  description: 'A test bot made with Eris',
+  owner: 'lold#4960',
+  prefix: '&'
 })
 
 bot.on('ready', () => {
-    console.log('dbot - discord responded with ready')
+  console.log('dbot - discord responded with ready')
 })
 
 bot.registerCommand('about', 'my name is dbot and I live in a computer. https://github.com/xshotD/dbot', {
-    description: 'who am I? run this to find out',
-    fullDescription: ':thinking:'
+  description: 'who am I? run this to find out',
+  fullDescription: ':thinking:'
 })
 
 bot.connect()
--- www/server.js
+++ www/server.js
@@ -3,11 +3,30 @@
 const e = require('express')
 const s = require('serve')
 const p = require('path')
-const bot = require('../bot/core')
+const b = require('eris')
+// Required for reading config
+const f = require('fs')
+// Please edit config.json with your token.
+const conf = JSON.parse(f.readFileSync('config.json', 'utf8'))
 // TODO: Show bot stats (may be a bit hard depending on if I can carry library stats to another file.)
 
 const a = e()
 
-bot.connect()
+const bot = new b.CommandClient(conf.token, {}, {
+  description: 'A test bot made with Eris',
+  owner: 'lold#4960',
+  prefix: '&'
+})
+
+bot.on('ready', () => {
+  console.log('dbot - discord responded with ready')
+})
+
+bot.registerCommand('about', 'my name is dbot and I live in a computer. https://github.com/xshotD/dbot', {
+  description: 'who am I? run this to find out',
+  fullDescription: ':thinking:'
+})
+
 a.use(s(p.join(__dirname, 'views')))
 a.listen(3000)
+bot.connect()
