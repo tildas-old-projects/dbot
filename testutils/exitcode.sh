c928f2c9f2569acca969d59be94358eba12a2e95
S Stewart <xshotD@users.noreply.github.com>
2017-04-13 20:45:24 -0500

    where'd the space go

--- testutils/exitcode.sh
+++ testutils/exitcode.sh
@@ -3,7 +3,7 @@ if [ $? == 0 ]
 then
   echo "✓ Test successful"
 else
-  echo "❌The test failed. Either check the issues or file a issue."
+  echo "❌ The test failed. Either check the issues or file a issue."
   exit 1
 fi
 
