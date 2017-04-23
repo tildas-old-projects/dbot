42e0a17b3679a7abe8dded05adbc61aa50b6a74d
S Stewart <hi@youneeda.pw>
2017-04-22 21:05:31 -0500

    ok

--- www/server.js
+++ www/server.js
@@ -3,30 +3,7 @@
 const e = require('express')
 const s = require('serve')
 const p = require('path')
-const b = require('eris')
-// Required for reading config
-const f = require('fs')
-// Please edit config.json with your token.
-const conf = JSON.parse(f.readFileSync('config.json', 'utf8'))
-// TODO: Show bot stats (may be a bit hard depending on if I can carry library stats to another file.)
-
 const a = e()
-
-const bot = new b.CommandClient(conf.token, {}, {
-  description: 'A test bot made with Eris',
-  owner: 'lold#4960',
-  prefix: '&'
-})
-
-bot.on('ready', () => {
-  console.log('dbot - discord responded with ready')
-})
-
-bot.registerCommand('about', 'my name is dbot and I live in a computer. https://github.com/xshotD/dbot', {
-  description: 'who am I? run this to find out',
-  fullDescription: ':thinking:'
-})
-
+// TODO: Show bot stats (will be hard, help needed)
 a.use(s(p.join(__dirname, 'views')))
 a.listen(3000)
-bot.connect()
