c7d102aa7541b60a50c9b4804766185434c905a5
S Stewart <hi@youneeda.pw>
2017-05-30 13:24:27 -0500

    [utils, site] add some stuff

--- testutils/exitcode.sh
+++ testutils/exitcode.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 if [ $? == 0 ]
 then
-  echo "✓ Test successful"
+  echo "✓ Test successful! Yay!"
 else
-  echo "❌ The test failed. Either check the issues or file a issue."
+  echo "❌ The test failed."
+  echo "❌ Please look at the error."
+  echo "❌ File a issue if it is with code."
   exit 1
 fi
 
---
+++ www/views/h4ck.js
@@ -0,0 +1,7 @@
+// h4ck.js
+// (c) dbot developer(s) 2017
+console.log('Hello! You discovered a nice hidden thing. Feel free to browse around the code!')
+console.log('Have any suggestions? File a issue at https://github.com/xshotD/dbot.')
+console.log('- a automated message from h4ck.js -')
+console.warn('Achievement unlocked: Hacker')
+console.warn('1337 G - Explore the devtools')
