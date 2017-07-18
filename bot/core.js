35bcb38a4821495f2e6bd8e7fda93edacc665db0
S Stewart <poweredby@linuxmail.org>
2017-07-18 17:38:34 -0500

    [bot] Update repo url

--- bot/core.js
+++ bot/core.js
@@ -1,5 +1,4 @@
 const e = require('eris')
-const os = require('os')
 
 // Please edit config.json with your token.
 const conf = JSON.parse(require('fs').readFileSync('config.json', 'utf8'))
@@ -7,7 +6,7 @@ const conf = JSON.parse(require('fs').readFileSync('config.json', 'utf8'))
 // You may change this desc, the owner, and prefix.
 const bot = new e.CommandClient(conf.token, {}, {
   description: conf.desc,
-  owner: conf.owner,
+  owner: 'Someone',
   prefix: conf.prefix
 })
 
@@ -15,7 +14,7 @@ bot.on('ready', () => {
   console.log('dbot - discord responded with ready')
 })
 
-bot.registerCommand('about', 'my name is dbot and I live in `' + os.hostname() + '` https://github.com/xshotD/dbot', {
+bot.registerCommand('about', 'my name is dbot and I live in `' + require('os').hostname() + '` https://github.com/tilda/dbot', {
   description: 'who am I? run this to find out',
   fullDescription: ':thinking:'
 })
@@ -25,7 +24,7 @@ bot.registerCommand('generic', 'generic sucks', {
   fullDescription: 'lmao im gonan die'
 })
 
-bot.on('messageCreate', (msg) => {
+bot.on('messageCreate', (msg, args, err) => {
   if (msg.content === conf.prefix + 'shutdown') {
     if (msg.author.id === conf.owner) {
       bot.createMessage(msg.channel.id, 'ok bye')
@@ -34,21 +33,13 @@ bot.on('messageCreate', (msg) => {
       bot.createMessage(msg.channel.id, 'no')
     }
   }
-})
-
-bot.on('messageCreate', (msg, args) => {
-  if (msg.content === conf.prefix + 'eval') {
+  if (msg.content === conf.prefix + 'exec') {
     if (msg.author.id === conf.owner) {
-      if (args.length === 0) {
-        bot.createMessage(msg.channel.id, 'You silly, you can\'t eval a empty string...')
+      const lol = require('shelljs').exec(args.toString())
+      if (lol.code !== 0) {
+        bot.createMessage(msg.channel.id, `Error! ${err.toString()}`)
       } else {
-       if (args.indexOf('bot.token') >= 0) {
-         bot.createMessage(msg.channel.id, ':rofl: Haha, no.')
-       } else {
-         bot.createMessage(msg.channel.id, '**Evaluated** - Results:\n```\n${eval(code)\n````')
-       }
-      }
-    }
-  }
-})
+	 bot.createMessage(msg.channel.id. `Success `\`\`\\n${lol}`)
+      }}})
+      
 bot.connect()
